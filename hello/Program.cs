3e3281b28aee6a90663947c73a553d0600ccc524
Auca Maillot <auca.maillot@farmacity.com.ar>
2023-05-09 12:20:13 -0300

    :sparkles: add simple input program

---
+++ hello/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.Write("Write something and I will repeat: ");
+            string inputUsuario = Console.ReadLine();
+            Console.WriteLine("{0}", inputUsuario);
+            // linea para el ejecutable exe
+            // Console.Read();
+        }
+
+    }
+}
