83304f4a28657cf9b121c0cb162312d9ce25ac72
Auca Maillot <aucacoyan@gmail.com>
2024-01-19 17:15:48 -0300

    :sparkles: add `hello_world`

---
+++ hello_world/Program.cs
@@ -0,0 +1,16 @@
+﻿
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.BackgroundColor = ConsoleColor.DarkGray;
+            // Console.Clear();
+            Console.WriteLine("Hello World");
+            // Console.Read();
+        }
+
+    }
+}
